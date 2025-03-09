f412c5a3a0563b59d4562883877252253ad6b7eb
Paul Grand <pgrand@gmail.com>
2025-03-09 15:07:11 +0000

    Update eslint cfg

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,23 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  rules: {
+    'react/no-unescaped-entities': 'off',
+    '@next/next/no-page-custom-font': 'off',
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
+
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  }),
+]
 
 export default eslintConfig;
